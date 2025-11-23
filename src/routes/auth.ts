935962b357c230a6c2592b30fb2badfc06546be6
michaelRS2002 <ramirez.michael@correounivalle.edu.co>
2025-11-23 17:39:04 -0500

    minor changes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,10 +7,7 @@ import { UserModel } from '../models/users';
 import GlobalDAO from '../dao/globalDAO';
 import crypto from 'crypto';
 import { sendResetPasswordEmail } from '../utils/mailer';
-import jwt from 'jsonwebtoken';
-import admin from '../firebase/firebaseAdmin';
-
-
+ 
 const router = express.Router();
 
 const userDao = new GlobalDAO('users', 'id');
